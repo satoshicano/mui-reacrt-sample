2c289b7b4d69a7972706d10fdd9fbad364a57f15
satoshicano <satoshicano@gmail.com>
2018-09-24 00:50:06 +0900

    install muicss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Button from "muicss/lib/react/button";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="primary">material button</Button>
       </div>
     );
   }
